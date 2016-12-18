baa0666954e58cba7f82eb2dc805bb5fba7030a8
iTeckHD <ituankha.tran+github@gmail.com>
2016-12-18 19:55:17 +0100

    json merge
    json merge of classes, spells and races to prevent big file editing

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,34 @@
 module.exports = function(grunt) {
 
   	grunt.initConfig({
+		//WATCH COMMAND
+	    watch: {
+	    	jshint: {
+	    		files: '_raw/js/*',
+	    		tasks: ['jshint', 'uglify', 'copy:js']
+	    	},
+	    	jsonlint : {
+	    		files: ['_raw/json/*', '_raw/json/**/*'],
+	    		tasks: ['jsonlint']
+	    	},
+	    	json_spells : {
+	    		files: ['_raw/json/spells/*'],
+	    		tasks: ['json_merge_spells']
+	    	},
+	    	json_classes : {
+	    		files: ['_raw/json/classes/*'],
+	    		tasks: ['json_merge_classes']
+	    	},
+	    	json_races : {
+	    		files: ['_raw/json/races/*'],
+	    		tasks: ['json_merge_races']
+	    	},
+	    	less : {
+	    		files: '_raw/less/*',
+	    		tasks: ['clean', 'less', 'cssmin', 'copy:css']
+	    	},
+	    },
+
   		//UTILITY
 	  	clean: ['_compiled/*'],
 	  	copy: {
@@ -60,26 +88,31 @@ module.exports = function(grunt) {
 
 		//JSON
 		jsonlint: {
-			json: {
+			all: {
 				src: [ '_raw/json/*.json' ]
+			},
+			spells: {
+				src: [ '_raw/json/spells/*.json' ]
+			}
+		}, 
+		json_merge_spells:{
+			build: {
+				src: '_raw/json/spells/*',
+				target: '_raw/json/spells.json',
 			}
 		},
-
-		//WATCH COMMAND
-	    watch: {
-	    	jshint: {
-	    		files: '_raw/js/*',
-	    		tasks: ['jshint', 'uglify', 'copy:js']
-	    	},
-	    	jsonlint : {
-	    		files: ['_raw/json/*', '_raw/json/**/*'],
-	    		tasks: ['jsonlint']
-	    	},
-	    	less : {
-	    		files: '_raw/less/*',
-	    		tasks: ['clean', 'less', 'cssmin', 'copy:css']
-	    	}
-	    }
+		json_merge_classes:{
+			build: {
+				src: '_raw/json/classes/*',
+				target: '_raw/json/classes.json',
+			}
+		},
+		json_merge_races:{
+			build: {
+				src: '_raw/json/races/*',
+				target: '_raw/json/races.json',
+			}
+		}
   	});
 
 	grunt.loadNpmTasks('grunt-contrib-less');
@@ -90,4 +123,18 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-jsonlint');
 	grunt.loadNpmTasks('grunt-contrib-copy');
+
+	grunt.registerMultiTask('json_merge_spells', mergeJson);
+	grunt.registerMultiTask('json_merge_classes', mergeJson);
+	grunt.registerMultiTask('json_merge_races', mergeJson);
+
+	function mergeJson(){
+		this.files.forEach(function(file) {
+			var a = file.src.map(function(path, fileIndex){ return '\r\n"' + (fileIndex + 1) + '": ' + grunt.file.read(path).trim(); });
+
+			if (grunt.file.exists(file.target))
+				grunt.file.delete(file.target);
+			grunt.file.write(file.target, "{" + a.join(', ') + "\r\n}");
+		});
+	}
 };
